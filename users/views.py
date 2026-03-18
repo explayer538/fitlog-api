4b87c32a8fa13beb59eb5f75b3a88c67e71e1593
explayer538 <franklineadubaah@gmail.com>
2026-03-17 22:30:29 -0400

    Add JWT authentication with register and login endpoints

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
